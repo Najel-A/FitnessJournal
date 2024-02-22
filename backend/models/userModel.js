7cf123203dde2a714b70b1087adcaac1b3160458
Najel Alarcon <najelalarcon@gmail.com>
2024-02-21 21:13:26 -0800

    Added jwt to backend

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -43,7 +43,28 @@ userSchema.statics.signup = async function(email, password) {
 
     return user;
 
+}
 
+// static login method
+userSchema.statics.login = async function(email, password) {
+    // checks if both fields are filled
+    if (!email || !password) {
+        throw Error('All fields must be filled');
+    }
+
+    const user = await this.findOne({ email }); 
+
+    if (!user){
+        throw Error('Invalid email address');
+    }
+
+    const match = await bcrpyt.compare(password, user.password);
+
+    if (!match){
+        throw Error('Invalid password');
+    }
+
+    return user;
 }
 
 module.exports = mongoose.model('User', userSchema);
